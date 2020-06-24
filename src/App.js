8f746ef062c532ff67d6c5f8d0f26fac488663f4
Marvin <marvinthompson@pursuit.org>
2020-06-23 22:19:57 -0400

    skeleton complete

--- src/App.js
+++ src/App.js
@@ -2,24 +2,31 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { Route, Switch } from 'react-router-dom'
+import Nav from '../src/components/Nav'
+import Home from '../src/components/Home/Home'
+import Project from '../src/components/Projects/Project'
+import About from '../src/components/About/About'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Nav/>
+      <Switch>
+
+        <Route exact path={"/"}>
+          <Home/>
+        </Route>
+
+        <Route path={"/about"}>
+          <About/>
+        </Route>
+
+        <Route path={"/projects"}>
+          <Project/>
+        </Route>
+
+      </Switch>
+
     </div>
   );
 }
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -5,7 +5,9 @@ const Nav = () => {
 
     return (
         <nav>
-
+            <NavLink exact to={"/"} activeClassName={"navItem"}>Home</NavLink>
+            <NavLink exact to={"/about"} activeClassName={"navItem"}>About</NavLink>
+            <NavLink exact to={"/projects"} activeClassName={"navItem"}>Projects</NavLink>
         </nav>
     )
 }
